c1801a5a73da57c57bd295a4d9806510b0d8c017
Mohamed El-Alafty <96385365+alafty@users.noreply.github.com>
2023-11-25 19:12:36 +0200

    Added given KB

---
+++ KB.pl
@@ -0,0 +1,5 @@
+food(1). %initial food level
+materials(0). %initial materials level
+energy(2). %initial energy level
+build1(1,2,1). %build1 requires 1 food, 2 materials, 1 energy
+build2(4,1,2). %build2 requires 4 food, 1 materials, 2 energy
